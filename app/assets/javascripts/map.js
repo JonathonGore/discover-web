a159f131a3990d40597e4a9eb6c2f14ed0258b26
Jonathon Gore <jackgore@hotmail.ca>
2017-08-27 18:07:53 -0500

    Added more map functionality

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,29 @@
+// Global coordinates var used for users location
+var coords;
+
 function initialize() {
+  coords = {lat: 43.471035, lng: -80.544545};
+
+  // Check to see if browser has geo support, then plot users position
+  if(navigator.geolocation) {
+    // If there is geo support at the position and display on map
+    navigator.geolocation.getCurrentPosition(displayOnMap);
+  }
+
+  // Adds users location and centers on it on the map
+  function displayOnMap(position){
+    coords.lat = position.coords.latitude;
+    coords.lng = position.coords.longitude;
+    // Having init map here will block and wait for the users location
+    //initMap();
+  };
+
+  initMap();
+}
+
+
+function initMap() {
+
   handler = Gmaps.build('Google');
   handler.buildMap({
       // pass in Google Maps API options in the provider object here
@@ -7,6 +32,9 @@ function initialize() {
 
         // Makes it so the map is not zoomed in so much with only a single marker
         maxZoom: 16,
+
+        // Set the default center of the map
+        center: new google.maps.LatLng(coords.lat, coords.lng),
 
         // Removes the street view option
         streetViewControl: false,
@@ -24,34 +52,23 @@ function initialize() {
     },
     function(){
 
-      // Add markers to map
-      // Eventually make a request to backend to fetch markers
-      markers = handler.addMarkers([
-        {
-          "lat": 43.471035,
-          "lng": -80.544545,
-          "infowindow": "hello!"
-        }
-      ]);
-
       // Check to see if browser has geo support, then plot users position
-      if(navigator.geolocation) {
-        // If there is geo support at the position and display on map
-        navigator.geolocation.getCurrentPosition(displayOnMap);
-      } else {
+      if(!navigator.geolocation) {
         // Otherwise fit the zoom the markers populated on the map
         handler.bounds.extendWith(markers);
         handler.fitMapToBounds();
       }
+
+      // Add markers to map
+      // Eventually make a request to backend to fetch markers
+      marker = handler.addMarker(
+        {
+          "lat": coords.lat,
+          "lng": coords.lng,
+          "infowindow": "hello!"
+        }
+      );
+      handler.map.centerOn(marker);
     }
   );
-
-  // Adds users location and centers on it on the map
-  function displayOnMap(position){
-    var marker = handler.addMarker({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    });
-    handler.map.centerOn(marker);
-  };
 }
