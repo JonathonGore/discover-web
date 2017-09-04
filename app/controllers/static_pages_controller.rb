d49845d77f1d98970e50cddf804bb7b67198a1be
Jonathon Gore <jackgore@hotmail.ca>
2017-09-04 19:28:54 -0400

    Use rails idiomatic ways for creating form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,16 @@ class StaticPagesController < ApplicationController
     @logged_out = !logged_in?
   end
 
+  def create
+
+    # Here we will validate our params and flash all errors
+    # move validation to helper method
+    if params[:longitude] == ""
+      puts "invalid"
+    end
+    print("test\n")
+  end
+
   def help
   end
 
